af932d02f4e92c9c1af3339de4452aab54ee3674
Aleksandr <62936324+Alekearl@users.noreply.github.com>
2021-01-18 13:17:54 +0200

    Update src/main/java/mate/academy/exception/DataProcessingException.java
    Co-authored-by: KseniiaMakarova <59459757+KseniiaMakarova@users.noreply.github.com>

--- src/main/java/mate/academy/exception/DataProcessingException.java
+++ src/main/java/mate/academy/exception/DataProcessingException.java
@@ -3,7 +3,7 @@ package mate.academy.exception;
 import java.sql.SQLException;
 
 public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String warning, SQLException e) {
+    public DataProcessingException(String warning, Throwable e) {
         super(warning, e);
     }
 }
